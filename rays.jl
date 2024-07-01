c6df75fc4d9d0e3c5cd36784828713ea0f4c9a6d
Matthew Ward <matthew.merrill.ward@gmail.com>
2024-07-01 17:12:12 -0600

    2d implementation complete

--- rays.jl
+++ rays.jl
@@ -4,10 +4,22 @@ module Rays2D
 
 using Infiltrator
 using LinearAlgebra
+using ImageFiltering
 
 struct Image
     intensities::Array
     contours::Array
+    gradients::Array # of gradient vectors
+end
+
+""" Constructs an Image from just intensities and contours. """
+function Image(intensities::Array, contours::Array)
+    all_gradients = imgradients(Float64.(intensities), KernelFactors.sobel, "symmetric")
+    gradients = Array{Vector}(undef, size(intensities)...)
+    for 𝐦 in Iterators.product(axes(intensities)...)
+        gradients[𝐦...] = [g[𝐦...] for g in all_gradients]
+    end
+    return Image(intensities, contours, gradients)
 end
 
 """ Checks that a given location 𝐦 is in the bounds of `image`. """
@@ -39,27 +51,54 @@ is_contour(𝐈::Image, 𝐦::Vector) = (0 != 𝐈.contours[floor.(Int, 𝐦)...
 """ Closest contour point 𝐜. θ in radians."""
 function closest_contour(𝐈::Image, 𝐦::Vector, θ::Number)
     step = [cos(θ), sin(θ)]
-    step /= norm(step) # Normalize step
+    step /= LinearAlgebra.norm(step) # Normalize step
 
-    @infiltrate
     while in_bounds(𝐈, 𝐦)
-        @infiltrate 𝐦[1] == 621
         if is_contour(𝐈, 𝐦)
             return floor.(Int, 𝐦)
         end
         𝐦 += step
-        @infiltrate 𝐦[1] >= 620
     end
 
     # If there is no contour before the edge of the image,
     # use `loc_in_image` to place ∞ values.
-    return floor.(Int, loc_in_image(𝐈, 𝐦))
+    return loc_in_image(𝐈, 𝐦)
 end
 
 """ Distance feature. """
 function distance(𝐈::Image, 𝐦::Vector, θ::Number)
     𝐜 = closest_contour(𝐈, 𝐦, θ)
-    return norm(𝐜 - 𝐦)
+    if (Inf in 𝐜) || (-Inf in 𝐜)
+        return Inf
+    end
+    return LinearAlgebra.norm(𝐜 - 𝐦)
+end
+
+""" Orientation feature. """ # TODO: seems to have some issues. demo
+function orientation(𝐈::Image, 𝐦::Vector, θ::Number)
+    𝐜 = closest_contour(𝐈, 𝐦, θ)
+    if (Inf in 𝐜) || (-Inf in 𝐜)
+        return NaN
+    end
+    return normalize(𝐈.gradients[𝐜...]) ⋅ [cos(θ), sin(θ)]
+end
+
+""" Norm feature. """
+function norm(𝐈::Image, 𝐦::Vector, θ::Number)
+    𝐜 = closest_contour(𝐈, 𝐦, θ)
+    if (Inf in 𝐜) || (-Inf in 𝐜)
+        return NaN
+    end
+    @infiltrate
+    return LinearAlgebra.norm(𝐈.gradients[𝐜...])
+end
+
+""" Distance difference feature. """
+function dist_difference(𝐈::Image, 𝐦::Vector, θ::Number, θ′::Number)
+    𝐜  = closest_contour(𝐈, 𝐦, θ )
+    𝐜′ = closest_contour(𝐈, 𝐦, θ′)
+    lanorm = LinearAlgebra.norm
+    return (lanorm(𝐜 - 𝐦) - lanorm(𝐜′ - 𝐦)) / lanorm(𝐜 - 𝐦)
 end
 
 end # module
--- test_rays.jl
+++ test_rays.jl
@@ -3,18 +3,80 @@ import .Rays2D
 using Images, ImageEdgeDetection, ImageDraw
 using ImageEdgeDetection: Percentile
 using JLD2
+using LocalFilters
+using ImageFiltering
+using LinearAlgebra
+using Plots
+using Infiltrator
 
-intensities = load_object("data/raw_slice_5.jld2")
+intensities = Float64.(load_object("data/raw_slice_5.jld2"))
+
+# Apply bilateral filters
+filters = [
+    img -> bilateralfilter(img, 1.0, 5), 
+    img -> bilateralfilter(img, 40/256, 8),
+    #img -> bilateralfilter(img, 20/256, 15)
+]
+
+# Apply filters and display results
+for f in filters
+    intensities = f(intensities)
+end
+intensities = Gray.(intensities)
+# Done filtering.
+
 canny(σ) = Canny(spatial_scale=σ, high=Percentile(98), low=Percentile(80))
 contours = detect_edges(intensities, canny(5))
 
+function cc_demo(𝐈, 𝐦, θ)
+    cc = Rays2D.closest_contour(𝐈, 𝐦, θ)
+
+    display(contours)
+    img = RGB.(𝐈.intensities)
+    draw!(img, Cross(Point(reverse(𝐦)...), 30), RGB{Float64}(1,0,0))
+    draw!(img, Cross(Point(reverse(cc)...), 30), RGB{Float64}(0,1,0)) |> display
+end
+
+function orientation_demo(𝐈, 𝐦, θ_target)
+    # Make image smoother
+    intens = imfilter(𝐈.intensities, Kernel.gaussian(16))
+    new_𝐈 = Rays2D.Image(intens, 𝐈.contours)
+    𝚯 = LinRange(0, 2π, 10000)
+    orientations = [Rays2D.orientation(new_𝐈, 𝐦, θ) for θ in 𝚯]
+    p = plot(
+        𝚯 ./ π,
+        orientations,
+        xlabel = "π radians",
+        ylabel = "Inner product with image gradient",
+        legend=false
+    )
+    # Plot the point we are interested in
+    scatter!(p, [θ_target/π], [Rays2D.orientation(new_𝐈, 𝐦, θ_target)])
+    display(p)
+end
+
+function norm_demo(𝐈, 𝐦, θ_target)
+    𝚯 = LinRange(0, 2π, 10000)
+    norms = [Rays2D.norm(𝐈, 𝐦, θ) for θ in 𝚯]
+    p = plot(
+        𝚯 ./ π,
+        norms,
+        xlabel = "π radians",
+        ylabel = "Gradient norm",
+        legend=false
+    )
+    # Plot the point we are interested in
+    scatter!(p, [θ_target/π], [Rays2D.norm(𝐈, 𝐦, θ_target)])
+    display(p)
+end
+
+# Testing orientation feature
 𝐈 = Rays2D.Image(intensities, contours)
-point = [480, 480]
-cc = Rays2D.closest_contour(𝐈, point, π/6)
+𝐦 = [445, 450]
+θ = .15 * π
+cc_demo(𝐈, 𝐦, θ)
+#plot(intensities) |> display
+orientation_demo(𝐈, 𝐦, θ)
+norm_demo(𝐈, 𝐦, θ)
 
-display(contours)
-img = RGB.(𝐈.intensities)
-draw!(img, Cross(Point(reverse(point)...), 30), RGB{Float64}(1,0,0))
-draw!(img, Cross(Point(reverse(cc)...), 30), RGB{Float64}(0,1,0))
-
-Rays2D.distance(𝐈, point, π/6)
+Rays2D.dist_difference(𝐈, 𝐦, 2π/3, π)
