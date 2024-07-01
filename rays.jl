8071c366c3b2c3802ed892f81ebf57ba36fcea52
Matthew Ward <matthew.merrill.ward@gmail.com>
2024-07-01 14:40:37 -0600

    closest_contour working

--- rays.jl
+++ rays.jl
@@ -2,50 +2,58 @@
 
 module Rays2D
 
+using Infiltrator
+using LinearAlgebra
+
 struct Image
     intensities::Array
     contours::Array
 end
 
 """ Checks that a given location 𝐦 is in the bounds of `image`. """
-function in_bounds(𝐈::Image, 𝐦::Tuple)
-    return 𝐦 == image_loc(𝐈, 𝐦)
+function in_bounds(𝐈::Image, 𝐦::Vector)
+    return 𝐦 == loc_in_image(𝐈, 𝐦)
 end
 
 """
 If the given point is outside the image bounds, represent the out of bounds dimensions as ∞ or -∞.
 """
-function loc_in_image(𝐈::Image, 𝐦::Tuple)
-    𝐦_new = 𝐦
+function loc_in_image(𝐈::Image, 𝐦::Vector)
+    𝐦 = Float64.(𝐦)
     image_dims = size(𝐈.intensities)
-    for (i, Nᵢ, 𝐦ᵢ) in enumerate(zip(image_dims, 𝐦))
-        if floor(𝐦ᵢ) < 1
-            𝐦_new[i] = -Inf
-        elseif floor(𝐦ᵢ) > Nᵢ
-            𝐦_new[i] = Inf
+    @assert length(image_dims) == length(𝐦)
+
+    for i in eachindex(image_dims)
+        if floor(𝐦[i]) < 1
+            𝐦[i] = -Inf
+        elseif floor(𝐦[i]) > image_dims[i]
+            𝐦[i] = Inf
         end
     end
-    return 𝐦_new
+    return 𝐦
 end
 
 """ Checks if a given location 𝐦 is a contour in the image 𝐈. """
-is_contour(𝐈::Image, 𝐦::Tuple) = (0 != 𝐈.contours[floor.(𝐦)...])
+is_contour(𝐈::Image, 𝐦::Vector) = (0 != 𝐈.contours[floor.(Int, 𝐦)...])
 
-""" Closest contour point 𝐜. """
-function closest_contour(𝐈::Image, 𝐦::Tuple, θ::Number)
-    step = (cos(θ), sin(θ))
-    step ./= norm(step) # Normalize step
+""" Closest contour point 𝐜. θ in radians."""
+function closest_contour(𝐈::Image, 𝐦::Vector, θ::Number)
+    step = [cos(θ), sin(θ)]
+    step /= norm(step) # Normalize step
 
+    @infiltrate
     while in_bounds(𝐈, 𝐦)
+        @infiltrate 𝐦[1] == 621
         if is_contour(𝐈, 𝐦)
-            return floor.(𝐦)
+            return floor.(Int, 𝐦)
         end
-        𝐦 .+= step
+        𝐦 += step
+        @infiltrate 𝐦[1] >= 620
     end
 
     # If there is no contour before the edge of the image,
     # use `loc_in_image` to place ∞ values.
-    return floor.(loc_in_image(𝐦))
+    return floor.(Int, loc_in_image(𝐦))
 end
 
 end # module
--- test_rays.jl
+++ test_rays.jl
@@ -1,6 +1,6 @@
 import .Rays2D
 
-using Images, ImageEdgeDetection
+using Images, ImageEdgeDetection, ImageDraw
 using ImageEdgeDetection: Percentile
 using JLD2
 
@@ -8,5 +8,11 @@ intensities = load_object("data/raw_slice_5.jld2")
 canny(σ) = Canny(spatial_scale=σ, high=Percentile(98), low=Percentile(80))
 contours = detect_edges(intensities, canny(5))
 
-Gray.(intensities) |> display
-Gray.(contours) |> display
+𝐈 = Rays2D.Image(intensities, contours)
+point = [480, 480]
+cc = Rays2D.closest_contour(𝐈, point, π/2)
+
+display(contours)
+img = RGB.(𝐈.intensities)
+draw!(img, Cross(Point(reverse(point)...), 30), RGB{Float64}(1,0,0))
+draw!(img, Cross(Point(reverse(cc)...), 30), RGB{Float64}(0,1,0))
